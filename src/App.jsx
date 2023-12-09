9f5e56d5dfbfc37b8b04febcba71f7d795b1c0e5
Vikram Naik <26vikramnaik@gmail.com>
2023-12-09 16:26:17 +0530

    redux set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  /* 
+    redux consists of following libries
+    - redux (core library, state management)
+    - immer (allows to mutate state)
+    - redux-thunk (handles async actions)
+    - reselects (simplifies reducer function) 
+  */
+  /* 
+    Setup cart slice
+    - application feature
+    - create features folders/cart
+    - create cartSlice.js
+  */
+  return <h2>Redux Toolkit</h2>
 }
-export default App;
+export default App
