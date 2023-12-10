301703bd7dce0e8f6334d0178fabd8058e07832f
Vikram Naik <26vikramnaik@gmail.com>
2023-12-10 21:41:19 +0530

    increase and decrease reducer function

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -20,8 +20,19 @@ const cartSlice = createSlice({
       const itemId = action.payload
       state.cartItems = state.cartItems.filter((items) => items.id !== itemId)
     },
+    increase: (state, { payload }) => {
+      // console.log(payload)
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      // console.log({ ...cartItem })
+      cartItem.amount = cartItem.amount + 1
+    },
+    decrease: (state, { payload }) => {
+      // console.log(payload)
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount - 1
+    },
   },
 })
 
-export const { clearCart, removeItem } = cartSlice.actions
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions
 export default cartSlice.reducer
