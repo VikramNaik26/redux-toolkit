e5d86c09bd683daa89a2c86820395bf15f0ec4d0
Vikram Naik <26vikramnaik@gmail.com>
2023-12-11 21:28:04 +0530

    ModalSlice

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CartContainer from './components/CartContainer'
 import Navbar from './components/Navbar'
 import { useDispatch, useSelector } from 'react-redux'
 import { calculateTotals } from './features/cart/cartSlice'
+import Modal from './components/Modal'
 function App() {
   /* 
     redux consists of following libries
@@ -19,14 +20,16 @@ function App() {
   */
 
   const { cartItems } = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(calculateTotals())
   }, [cartItems])
-  
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
